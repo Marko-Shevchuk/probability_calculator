ccb8335eb3e583b7ed8da7610193195c1e768818
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2024-03-03 23:57:40 +0200

    Add first calculator page logic

--- Assets/Scripts/Calculator1.cs
+++ Assets/Scripts/Calculator1.cs
@@ -23,7 +23,7 @@ public class Calculator1 : MonoBehaviour
     public TMP_InputField outputAxorB;
     public TMP_InputField outputAnorB;
     public Button calculateButton;
-
+    public TMP_Text errorText;
     void Start()
     {
         calculateButton.onClick.AddListener(CalculateProbability);
@@ -31,6 +31,77 @@ public class Calculator1 : MonoBehaviour
 
     void CalculateProbability()
     {
+        // Retrieve input values
+        float a = GetValidatedProbability(inputA.text);
+        float b = GetValidatedProbability(inputB.text);
+        float notA = GetValidatedProbability(inputNotA.text);
+        float notB = GetValidatedProbability(inputNotB.text);
+        float aAndB = GetValidatedProbability(inputAandB.text);
+        float aOrB = GetValidatedProbability(inputAorB.text);
+        float aXorB = GetValidatedProbability(inputAxorB.text);
+        float aNorB = GetValidatedProbability(inputAnorB.text);
         
+        if (a == -1 | b == -1)
+        {
+            if (a == -1 & notA != -1) a = 1 - notA;
+            if (b == -1 & notB != -1) b = 1 - notB;
+            if (aOrB == -1 & aNorB != -1) aOrB = 1 - aNorB;
+        }
+        if (a == -1 | b == -1)
+        {
+           
+            if (a != -1 && aAndB != -1)
+            {
+                b = aAndB / a;
+            }
+            if (b != -1 && aAndB != -1)
+            {
+                a = aAndB / b;
+            }
+            if (a != -1 && aOrB != -1)
+            {
+                b = (aOrB - a)/(1 - a);
+            }
+            if (b != -1 && aOrB != -1)
+            {
+                a = (aOrB - b) / (1 - b);
+            }
+        }
+        if (a == -1 | b == -1)
+        {
+            outputA.text = "Failed to calculate based on given data.";
+        }
+        else
+        {
+            notA = 1 - a;
+            notB = 1 - b;
+            aAndB = a * b;
+            aOrB = a + b - a * b;
+            aXorB = a + b - 2 * a * b;
+            aNorB = 1 - aOrB;
+        }
+
+        outputA.text = a.ToString();
+        outputB.text = b.ToString();
+        outputNotA.text = notA.ToString();
+        outputNotB.text = notB.ToString();
+        outputAandB.text = aAndB.ToString();
+        outputAorB.text = aOrB.ToString();
+        outputAxorB.text = aXorB.ToString();
+        outputAnorB.text = aNorB.ToString();
+    }
+
+    float GetValidatedProbability(string input)
+    {
+        float value = 0;
+        if (float.TryParse(input, out value))
+        {
+            if ((value >= 0) & (value <= 1))
+            {
+                return value;
+            }
+        }
+        errorText.text = "Input value not a number or not within [0,1].";
+        return -1;
     }
 }
